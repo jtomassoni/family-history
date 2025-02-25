11f6bb9f5b110701fec96f3ccd78b5c0fe9021c9
James Tomassoni <jtomassoni@gmail.com>
2025-02-25 09:47:49 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h1>Family History Timeline</h1>
+  </div>
+</template>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+</style>
