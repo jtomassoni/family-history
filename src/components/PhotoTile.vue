4449b241f0718566d585bba28d94cd6227c19ff6
James Tomassoni <jtomassoni@gmail.com>
2025-02-27 17:18:19 -0700

    fix urls

--- src/components/PhotoTile.vue
+++ src/components/PhotoTile.vue
@@ -1,21 +1,45 @@
 <template>
   <div class="photo-tile-container">
     <div class="photo-content">
-      <div class="photo-image-container">
+      <div class="photo-image-container" @click="openModal">
         <img :src="photo.url" :alt="photo.externalName" class="photo-image" />
       </div>
       <div class="photo-info">
         <h3 class="photo-title">{{ photo.externalName }}</h3>
         <p>{{ photo.description }}</p>
+        <p v-if="photo.date"><strong>Date:</strong> {{ formatDate(photo.date) }}</p>
+        <p v-if="photo.uploadedAt"><strong>Uploaded:</strong> {{ formatDate(photo.uploadedAt) }}</p>
       </div>
+    </div>
+
+    <!-- Image Modal -->
+    <div v-if="isModalOpen" class="image-modal" @click="closeModal">
+      <img :src="photo.url" :alt="photo.externalName" class="modal-image" />
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import "../styles/PhotoTile.css";
 
 defineProps({
   photo: Object
 });
+
+const isModalOpen = ref(false);
+
+const openModal = () => {
+  isModalOpen.value = true;
+};
+
+const closeModal = () => {
+  isModalOpen.value = false;
+};
+
+// Helper function to format dates
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString(undefined, { year: "numeric", month: "long", day: "numeric" });
+};
 </script>
--- src/components/SwipeHint.vue
+++ src/components/SwipeHint.vue
@@ -5,9 +5,10 @@
         Swipe left or right to navigate through history.<br>
         You can also use the
         <span class="datepicker-text">
-          <span class="calendar-icon">🗓️</span> date picker
+          <span class="calendar-icon">📅</span> date picker
         </span>
-        at the top!
+        at the top!<br><br>
+        <strong>Tap an image to enlarge it!</strong>
       </p>
       <div class="arrow-container">
         <span class="arrow arrow-left">⟵</span>
@@ -24,11 +25,18 @@ import "../styles/SwipeHint.css";
 const showHint = ref(false);
 
 onMounted(() => {
+  console.log("Mounted: Checking screen width...");
+  
   if (window.innerWidth < 768) {
+    console.log("Showing swipe hint on mobile.");
     showHint.value = true;
+    
     setTimeout(() => {
+      console.log("Hiding swipe hint...");
       showHint.value = false;
     }, 4000); // Hide after 4 seconds
+  } else {
+    console.log("Not mobile: Swipe hint will not show.");
   }
 });
 </script>
