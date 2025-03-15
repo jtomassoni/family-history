a5e102076d8178d1102aef2ac40f1a9b65557ef5
James Tomassoni <jtomassoni@gmail.com>
2025-03-14 20:50:15 -0600

    config update for mobile testing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 5173,     // or whichever port you prefer
+  },
 })
